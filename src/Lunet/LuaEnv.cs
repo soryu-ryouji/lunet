7bf7fc8743a5b4c73d9dcf7877ff0c8b7f26d1f6
ryouji <helloryouji@outlook.com>
2024-10-24 11:09:59 +0800

    init repo

---
+++ src/Lunet/LuaEnv.cs
@@ -0,0 +1,14 @@
+﻿namespace Lunet;
+
+public class LuaEnv : IDisposable
+{
+    public object[] DoString(byte[] chunk, string chunkName = "chunk", LuaTable env = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Lunet/LuaTable.cs
@@ -0,0 +1,5 @@
+namespace Lunet;
+
+public class LuaTable
+{
+}
